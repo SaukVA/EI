de86160b65cf1dc2cdddebeaad9ed6432dedf293
Saul Verdu Aparicio <kowsva16@gmail.com>
2021-04-11 14:05:20 +0200

    imprlementadas funciones de idexadorHash

--- P2/lib/indexadorHash.cpp
+++ P2/lib/indexadorHash.cpp
@@ -1,5 +1,6 @@
 #include "indexadorHash.h"
 
+
 ostream& operator<<(ostream& s, const IndexadorHash& p) {
     s << "Fichero con el listado de palabras de parada: " << p.ficheroStopWords << endl;
     s << "Tokenizador: " << p.tok << endl;
@@ -12,7 +13,7 @@ ostream& operator<<(ostream& s, const IndexadorHash& p) {
 }
 
 IndexadorHash::IndexadorHash(const string& fichStopWords, const string& delimitadores, const bool& detectComp, const bool& minuscSinAcentos, const string& dirIndice, const int& tStemmer, const bool& almEnDisco, const bool& almPosTerm){
-
+    //...
     // ifstream file;
     // string str;
 
@@ -35,16 +36,52 @@ IndexadorHash::IndexadorHash(const string& directorioIndexacion){
     //...
 }
 
-IndexadorHash::IndexadorHash(const IndexadorHash&){
-    //...
+IndexadorHash::IndexadorHash(const IndexadorHash& index){
+    if(this != &index){
+        indice = index.indice;
+        indiceDocs = index.indiceDocs;
+        informacionColeccionDocs = index.informacionColeccionDocs;
+        infPregunta = index.infPregunta;
+        almacenarEnDisco =index.almacenarEnDisco;
+        almacenarPosTerm = index.almacenarPosTerm;
+        tipoStemmer = index.tipoStemmer;
+        directorioIndice = index.directorioIndice;
+        pregunta = index.pregunta;
+        indicePregunta = index.indicePregunta;
+        ficheroStopWords = index.ficheroStopWords;
+        stopWords = index.stopWords;
+        tok = index.tok;
+    }
 }
         
 IndexadorHash::~IndexadorHash(){
-    //...
+    indice.clear();
+    indiceDocs.clear();
+    almacenarEnDisco = false;
+    almacenarPosTerm = false;
+    tipoStemmer = 0;
+    directorioIndice = "";
+    pregunta = "";
+    indicePregunta.clear();
+    ficheroStopWords = "";
 }
         
-IndexadorHash& IndexadorHash::operator= (const IndexadorHash&){
-    //...
+IndexadorHash& IndexadorHash::operator= (const IndexadorHash& index){
+    if(this != &index){
+        indice = index.indice;
+        indiceDocs = index.indiceDocs;
+        informacionColeccionDocs = index.informacionColeccionDocs;
+        infPregunta = index.infPregunta;
+        almacenarEnDisco =index.almacenarEnDisco;
+        almacenarPosTerm = index.almacenarPosTerm;
+        tipoStemmer = index.tipoStemmer;
+        directorioIndice = index.directorioIndice;
+        pregunta = index.pregunta;
+        indicePregunta = index.indicePregunta;
+        ficheroStopWords = index.ficheroStopWords;
+        stopWords = index.stopWords;
+        tok = index.tok;
+    }
 }
 
 bool IndexadorHash::Indexar(const string& ficheroDocumentos){
@@ -58,14 +95,25 @@ bool IndexadorHash::IndexarDirectorio(const string& dirAIndexar){
 }
 
 bool IndexadorHash::GuardarIndexacion() const{
-    return true;
+    //...
+    return false;
 }
+
 bool IndexadorHash::RecuperarIndexacion (const string& directorioIndexacion){
-    return true;
+    //...
+    return false;
 }
 
 void IndexadorHash::ImprimirIndexacion() const{
-    //...
+    cout << "Terminos indexados: " << endl;
+    for(auto it = indiceDocs.begin(); it != indiceDocs.end(); ++it){
+        cout << it->first << "\t" << it->second << endl;
+    }
+
+    cout << "Documentos indexados: " << endl;
+    for(auto it = indice.begin(); it != indice.end(); ++it){
+        cout << it->first << "\t" << it->second << endl;
+    }
 }
 
 bool IndexadorHash::IndexarPregunta(const string& preg){
@@ -74,13 +122,21 @@ bool IndexadorHash::IndexarPregunta(const string& preg){
 }
 
 bool IndexadorHash::DevuelvePregunta(string& preg) const{
-    //...
+    if(!pregunta.empty()){
+        preg = pregunta;
+        return true;
+    }
     return false;
 }
 
 bool IndexadorHash::DevuelvePregunta(const string& word, InformacionTerminoPregunta& inf) const{
-    //...
-    return false;
+    if(indicePregunta.find(word) == indicePregunta.end()){
+        return false;
+    }
+    else{
+        inf = indicePregunta.find(word)->second;
+        return true;
+    }
 }
 
 bool IndexadorHash::DevuelvePregunta(InformacionPregunta& inf) const{
@@ -89,15 +145,24 @@ bool IndexadorHash::DevuelvePregunta(InformacionPregunta& inf) const{
 }
 
 void IndexadorHash::ImprimirIndexacionPregunta(){
-    //...
+    cout << "Pregunta indexada: " << pregunta << endl;
+    cout << "Terminos indexados en la pregunta: " << endl;
+    for(auto it = indicePregunta.begin(); it != indicePregunta.end(); ++it){
+        cout << it->first << "\t" << it->second << endl;
+    }
+    cout << "Informacion de la pregunta: " << infPregunta << endl;
 }
 
 void IndexadorHash::ImprimirPregunta(){
-    //...
+    cout << "Pregunta indexada: " << pregunta << endl;
+    cout << "Informacion de la pregunta: " << infPregunta << endl;
 }
 
 bool IndexadorHash::Devuelve(const string& word, InformacionTermino& inf) const{
-    //...
+    if(Existe(word)){
+        inf = indice.find(word)->second;
+        return true;
+    }
     return false;
 }
 
@@ -107,13 +172,11 @@ bool IndexadorHash::Devuelve(const string& word, const string& nomDoc, InfTermDo
 }
 
 bool IndexadorHash::Existe(const string& word) const{
-    //...
-    return false;
+    return (indice.find(word) != indice.end());
 }
 
 bool IndexadorHash::Borra(const string& word){
-    //...
-    return false;
+    return (indice.erase(word) != 0);
 }
 
 bool IndexadorHash::BorraDoc(const string& nomDoc){
@@ -122,93 +185,118 @@ bool IndexadorHash::BorraDoc(const string& nomDoc){
 }
 
 void IndexadorHash::VaciarIndiceDocs(){
-    //...
+    indice.clear();
+    indiceDocs.clear();
 }
 
 void IndexadorHash::VaciarIndicePreg(){
-    //...
+    indicePregunta.clear();
 }
 
 bool IndexadorHash::Actualiza(const string& word, const InformacionTermino& inf){
-    //...
+    
+    if(Existe(word)){
+        indice[word] = inf;
+        return true;
+    }
     return false;
 }
 
 bool IndexadorHash::Inserta(const string& word, const InformacionTermino& inf){
-    //...
+    if(!Existe(word)){
+        indice[word] = inf;
+        return true;
+    }
     return false;
 }
 
 int IndexadorHash::NumPalIndexadas() const{
-    //...
+    return indice.size();
 }
 
 string IndexadorHash::DevolverFichPalParada () const{
-    //...
+    return ficheroStopWords;
 }
 
 void IndexadorHash::ListarPalParada() const{
-    //...
+    for(auto it = stopWords.begin(); it != stopWords.end(); ++it){
+        cout << (*it) << endl;
+    }
 }
 
 int IndexadorHash::NumPalParada() const{
-    //...
+    return stopWords.size();
 }
 
 string IndexadorHash::DevolverDelimitadores () const{
-    //...
+    return tok.DelimitadoresPalabra();
 }
 
 bool IndexadorHash::DevolverCasosEspeciales () const{
-    //...
-    return false;
+    return tok.CasosEspeciales();
 }
 
 bool IndexadorHash::DevolverPasarAminuscSinAcentos () const{
-    //...
-    return false;
+    return tok.PasarAminuscSinAcentos();
 }
 
 bool IndexadorHash::DevolverAlmacenarPosTerm () const{
-    //...
-    return false;
+    return almacenarPosTerm;
 }
 
 string IndexadorHash::DevolverDirIndice () const{
-    //...
+    return directorioIndice;
 }
 
 int IndexadorHash::DevolverTipoStemming () const{
-    //...
+    return tipoStemmer;
 }
 
 bool IndexadorHash::DevolverAlmEnDisco () const{
-    //...
-    return false;
+    return almacenarEnDisco;
 }
 
 void IndexadorHash::ListarInfColeccDocs() const{
-    //...
+    cout << informacionColeccionDocs << endl;
 }
 
 void IndexadorHash::ListarTerminos() const{
-    //...
+    for(auto it = indice.begin(); it != indice.end(); ++it){
+        cout << it->first << "\t" << it->second << endl;
+    }
 }
 
 bool IndexadorHash::ListarTerminos(const string& nomDoc) const{
-    //...
-    return false;
+    if(indiceDocs.find(nomDoc) == indiceDocs.end()){
+        return false;
+    }
+    else{
+        //...
+        return true;
+    }
 }
 
 void IndexadorHash::ListarDocs() const{
-    //...
+    for(auto it = indiceDocs.begin(); it != indiceDocs.end(); ++it){
+        cout << it->first << "\t" << it->second << endl;
+    }
 }
 
 bool IndexadorHash::ListarDocs(const string& nomDoc) const{
-    //...
-    return false;
+    if(indiceDocs.find(nomDoc) == indiceDocs.end()){
+        return false;
+    }
+    else{
+        cout << indiceDocs.find(nomDoc)->first << "\t" << indiceDocs.find(nomDoc)->second << endl;
+        return true;
+    }
 }
 
 IndexadorHash::IndexadorHash(){
-    //...
+    pregunta = "";
+    ficheroStopWords = "";
+    directorioIndice= "";
+    tipoStemmer = 0;
+    almacenarEnDisco = false;
+    almacenarPosTerm = false;
 }
